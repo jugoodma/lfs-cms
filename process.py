48f6c225b057fc78ef9b8479443cbb007ccc9e8b
Justin Goodman <jugoodma@users.noreply.github.com>
2017-07-24 20:36:17 -0400

    Update process.py
    added cgi

--- process.py
+++ process.py
@@ -1,9 +1,18 @@
-import re
-incommand = input("Input: ")
+import re #eeeeeeeeeeeeeeee
+import cgi
+
+data = cgi.FieldStorage().getvalue('userInput') # <input type="text" name="userInput">
+# data = input("Input: ")
 outstring =""
 pattern = '^[a-z0-9]$'
+
+# clear the output.txt everytime, and whenever we present the html form 
+# to the user we will just copy the output.txt and put it in the form field 
+# (that way they're just editing their original text every time and not 
+# just a blank field)
+
 f = open("output.txt","w+")
-for s in incommand:
+for s in data:
     if re.match(pattern, s):
         outstring+= s
 f.write(outstring)
