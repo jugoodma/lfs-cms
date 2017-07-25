0a5b16513b268e5047e8760c7ed94f6c8310eb44
Cliff Bakalian <Ambrosiushunter@gmail.com>
2017-07-25 02:49:16 -0400

    the set methods work now. output.css must exist though. and there are probably a few bugs for set and add but right now they work if you know how to use them. python 3 compatible. there is also a method to make a generic css file.

--- htmlwriter.py
+++ htmlwriter.py
@@ -22,9 +22,7 @@ def main():
             
 def add(command):
     tag = command.split(' ',1)[0]
-    command = cmmand[len(tag):]
-    name = command.split(' ',1)[0]
-    value = command[len(name):]
+    value = command[len(tag):]
     f = open("output.html","r+")
     line = next(f)
     output = line;
@@ -42,21 +40,69 @@ def add(command):
     f.close()
 
 def set(command):
-    f = open("out.css","w+")
-    css = ""
-    temp = command.split(' ',1)[0]
-    for line in f:
-        if temp in line:
-            css += temp + ":" + command[len(temp):]
+    name = False
+    tag = command.split(' ',1)[0]
+    if "-" in tag:
+        ID = tag.split('-',1)[0]
+        value = command[len(tag):]
+        tag = tag.split('-',1)[1]
+    else:
+        value = command[len(tag):]
+    f = open("output.css","r+")
+    line = next(f)
+    output = ""
+    added = False
+    count = len(line)
+    if not name:
+        while not "}" in line:
+            if tag in line:
+                output+="\t"+tag+":"+value+"\n"
+                line = next(f)
+                count+=len(line)
+                added = True
+            output += line
+            line = next(f)
+            count += len(line)
+        if not added:
+            output+="\t"+tag+":"+value+"\n}\n"
         else:
-            css+=line
-    f.write(css)
+            output+="}\n"
+        f.seek(count)
+        for line in f:
+            output+=line
+    else:
+        while not ID in line:
+            output+=line
+            count += 1
+        while not "}" in line:
+            output += line
+            if tag in line:
+                output+="\t"+tag+":"+value+"\n}"
+                line = next(f)
+                count+=len(line)
+                added = True
+            line = next(f)
+            count += len(line)
+        if not added:
+            output+="\t"+tag+":"+value+"\n}\n"
+        else:
+            output+="}\n"
+        f.seek(count)
+        for line in f:
+            output+=line
+    f.close()
+    f = open("output.css","w+")
+    f.write(output)
     f.close()
 
 def makeGeneric():
     f = open("output.html","w+")
     html = '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="UTF-8">\n\t\t<link type="text/css" rel="stylesheet" href="page.css"/>\n\t\t<title>Title Page</title>\n\t</head>\n\t<body>\n\t\t<section>\n\t\t\t<h1>\n\t\t\t\t<strong>This is A Title</strong>\n\t\t\t</h1>\n\t\t<section>\n\t\t\tLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed euismod risus. Aenean at imperdiet nulla. Vestibulum finibus ligula sem, at sollicitudin metus commodo nec. Vestibulum sagittis turpis massa, placerat pellentesque eros pretium non. In consectetur vestibulum semper. Proin eget sapien consectetur, faucibus leo et, condimentum nunc. Cras ultricies malesuada scelerisque. Pellentesque finibus scelerisque bibendum. Aliquam sollicitudin nisl purus, vel porta massa porttitor a. Donec viverra tempor auctor.\n\t\t</section>\n\t</body>\n</html>'
     f.write(html)
+    f.close()
+    f = open("output.css","w+")
+    css = "body {\n\tfont-family: 'Raleway', sans-serif;\n}"
+    f.write(css)
     f.close()
     
 main()
