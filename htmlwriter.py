40ebda41bc24be6c98be0d8c72de540789c0fdb0
Cliff Bakalian <Ambrosiushunter@gmail.com>
2017-07-25 01:52:45 -0400

    this can make a output.html webpage which is very generic and must exist for this module to work. This also includes a working add command which will add paragraphs or headers to output.html

--- htmlwriter.py
+++ htmlwriter.py
@@ -19,21 +19,27 @@ def main():
         elif re.match('^set *',command):
             if set(command[4:]) == 0:
                 break;
-
+            
 def add(command):
-    f = open("output.txt","a+")
     tag = command.split(' ',1)[0]
+    command = cmmand[len(tag):]
+    name = command.split(' ',1)[0]
+    value = command[len(name):]
+    f = open("output.html","r+")
+    line = next(f)
+    output = line;
+    while not "</body>" in line:
+        output += line
+        line = next(f)
     if tag == "paragraph":
-        f.write("<p>" + command[len(tag):] + "</p>")
+        output+="\t\t<p>" + value + "</p>\n"
     elif tag == "header":
-        f.write("<header>" + command[len(tag):] + "</header>")
-    else:
-        print("invalid command")
-        f.close()
-        return 0
-    f.write("\n")
+        output+="\t\t<header>" + value + "</header>\n"
+    output+="\t</body>\n</html>"
     f.close()
-    return 1;
+    f = open("output.html","w+")
+    f.write(output)
+    f.close()
 
 def set(command):
     f = open("out.css","w+")
@@ -41,10 +47,16 @@ def set(command):
     temp = command.split(' ',1)[0]
     for line in f:
         if temp in line:
-            css += temp + ":" + command[len(temp):])
+            css += temp + ":" + command[len(temp):]
         else:
             css+=line
     f.write(css)
+    f.close()
+
+def makeGeneric():
+    f = open("output.html","w+")
+    html = '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="UTF-8">\n\t\t<link type="text/css" rel="stylesheet" href="page.css"/>\n\t\t<title>Title Page</title>\n\t</head>\n\t<body>\n\t\t<section>\n\t\t\t<h1>\n\t\t\t\t<strong>This is A Title</strong>\n\t\t\t</h1>\n\t\t<section>\n\t\t\tLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed euismod risus. Aenean at imperdiet nulla. Vestibulum finibus ligula sem, at sollicitudin metus commodo nec. Vestibulum sagittis turpis massa, placerat pellentesque eros pretium non. In consectetur vestibulum semper. Proin eget sapien consectetur, faucibus leo et, condimentum nunc. Cras ultricies malesuada scelerisque. Pellentesque finibus scelerisque bibendum. Aliquam sollicitudin nisl purus, vel porta massa porttitor a. Donec viverra tempor auctor.\n\t\t</section>\n\t</body>\n</html>'
+    f.write(html)
     f.close()
     
 main()
