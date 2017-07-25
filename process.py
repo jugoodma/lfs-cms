5ddc20515cb1a214ac941bcb68576846135eb7db
Cliff Bakalian <Ambrosiushunter@gmail.com>
2017-07-24 20:53:54 -0400

    im just dumb. its all good now

--- process.py
+++ process.py
@@ -5,14 +5,13 @@ data = cgi.FieldStorage().getvalue('userInput') # <input type="text" name="userI
 # data = input("Input: ")
 outstring =""
 pattern = '^[a-z0-9]$'
-
 # clear the output.txt everytime, and whenever we present the html form 
 # to the user we will just copy the output.txt and put it in the form field 
 # (that way they're just editing their original text every time and not 
 # just a blank field)
 
 f = open("output.txt","w+")
-for s in outstring:
+for s in data:
     if re.match(pattern, s):
         outstring+= s
 f.write(outstring)
